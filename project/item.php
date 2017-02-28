6961b71cdcfc5062ec349e16b81ba3b5fbda4b89
alp13d <alp13d@acu.edu>
2017-02-27 21:16:19 -0600

    Add files via upload

--- project/item.php
+++ project/item.php
@@ -69,8 +69,96 @@
 					</table>
 				</div>
 					<?php
-     				}
+     		}
+			
 		}
-					?>
-</body>
+		?>
+
+
+		<?php
+		//REVIEWS
+			/*$link =  mysql_connect("localhost","root","");
+
+			if (!$link) {
+    			printf("Connect failed: %s\n", mysql_error());
+    			exit();
+			}
+
+			$db_selected = mysql_select_db("amazion", $link);
+
+			if (!$db_selected) {
+    			die("Database selection failed: " . mysql_error());
+			}*/
+			function reviewProcessing() {
+    			$uploadOk = 1;
+				$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+				move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
+
+				$link = mysql_connect("localhost","root","");
+				if (!$link) {
+				printf("Connect failed: %s\n", mysql_error());
+				exit();
+				}
+				$db_selected = mysql_select_db("amazion", $link);
+				if (!$db_selected) {
+				die("Database selection failed: " . mysql_error());
+				}
+
+				$user = $_POST['username'];
+				$rate = $_POST['rate'];
+				$review = $_POST['review'];
+				$id = 1; //not sure about this
+
+
+				$sql = "INSERT INTO review (id, user, rate, review) VALUES ('$id', '$user', '$rate', '$review')";
+
+				if (!mysql_query($sql)) {
+				die('Error: ' . mysql_error());
+				}
+				mysql_close();
+			}
+		?>
+			</body>
+
+			<footer>
+
+	<h3>Submit a Review</h3>
+		<form method="post" action="reviewProcessing()" name="add_review" enctype="multipart/form-data">
+
+			Username: <input type="text" name="user" id="add_name" /> <br/>
+			Rating:<input type="text" name="rate" id="add_rate" /> <br/>
+			Review:<br/>
+			<textarea rows="10" cols="100" name="review"> </textarea><br/>
+			<input type="submit" value="Submit Review" />
+		</form>
+            
+           
+           <?php
+
+			$sql = "SELECT id, user, rate, review FROM review";
+
+			$result = mysql_query("select * from review");;
+			
+
+
+		while($row = mysql_fetch_assoc($result)) 
+		{
+				?>
+     			<div id="review">
+     				<table border="1" id="review1">
+					<tr>
+						<td colspan="2"><?php echo $row["user"];?></td>
+					</tr>
+					<tr>
+						<td colspan="2"><?php echo $row["rate"];?></td>
+					</tr>
+					<tr>
+						<td colspan="2"><?php echo $row["review"];?></td>
+					</tr>
+					</table>
+				</div>
+					<?php
+		}
+		?>
+</footer>
 </html>
